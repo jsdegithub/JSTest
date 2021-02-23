1624fa45d2c6aa62f8a8e2500221cda3f7ccfc31
jsdegithub <def_myself@qq.com>
2021-02-24 02:22:53 +0800

    update

---
+++ css-test/postcss_test/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    plugins: [
+        require("autoprefixer"),
+        // require("postcss-import"),
+        // require("cssnano"),
+        // require("postcss-cssnext"),
+        // require("precss"),  //precss包含autoprefixer,所以安装了precss就不用再安装autoprefixer了
+    ],
+};
